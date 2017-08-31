51f895429775c8edaa24d4df7ee16ebf555d1fb5
zhaojiong <13918255827@139.com>
2017-08-31 16:43:19 +0800

    Signed-off-by: zhaojiong <13918255827@139.com>

---
+++ src/com/zhao/string/Test5.java
@@ -0,0 +1,52 @@
+/**
+ * Test.java
+ * Created at 2017年8月30日
+ * Created by zhaojiong
+ * Copyright (C) 2017 SHANGHAI VOLKSWAGEN, All rights reserved.
+ */
+package com.zhao.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * ClassName: Test
+ * </p>
+ * <p>
+ * Description: TODO
+ * </p>
+ * <p>
+ * Author: zhaojiong
+ * </p>
+ * <p>
+ * Date: 2017年8月30日
+ * </p>
+ */
+public class Test5 {
+	
+ 
+    public static void main(String[] args) throws IOException    {
+    	System.out.println("Begin:");
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    	String str = null; 
+    	str = br.readLine(); 
+    	System.out.println("your value is :"+str); 
+    List<String> list = new ArrayList<String>();
+    int i = 0 ;
+    while(true){
+    	list.add(String.valueOf(i++).intern());
+    }
+    
+    }
+	
+    
+
+  
+    
+    
+    
+}
